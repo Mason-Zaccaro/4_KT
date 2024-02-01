17bbf680a63d3853a138337f27b9177ecc9a1c75
Mason-Zaccaro <65304987+Mason-Zaccaro@users.noreply.github.com>
2024-02-01 14:22:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            MessageBox.Show("Пойдешь на встречу?", "Опрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question); 
+        }
+    }
+}
