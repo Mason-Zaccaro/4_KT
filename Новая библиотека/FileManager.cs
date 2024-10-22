77820cd03710378a027a7779f6984b3b2f150afc
Mason Zaccaro <65304987+Mason-Zaccaro@users.noreply.github.com>
2024-10-22 15:49:48 +0300

    Тест

---
+++ Новая библиотека/FileManager.cs
@@ -0,0 +1,15 @@
+﻿namespace New_library
+{
+    public class FileManager
+    {
+        public static void WriteToFile(string filePath, string content)
+        {
+            File.WriteAllText(filePath, content);
+        }
+
+        public static string ReadFromFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+    }
+}
